946e2f7f3d118fa2ba26e38e755417923a63f410
SecretariatV <oliver.b25.f@gmail.com>
2024-06-07 17:55:26 +0900

    create basic structure

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5000,
+    host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@features": "/src/features",
+      "@pages": "/src/pages",
+      "@styles": "/src/styles",
+      "@utils": "/src/utils",
+    },
+  },
+});

69b641f82521886a04f756c9d370fb3e4f192dce
secretariatv <oliver.b25.f@gmail.com>
2024-06-27 10:12:23 +0000

    add: errorboundary

--- src/utils/typeUtils.ts
+++ src/utils/typeUtils.ts
@@ -1,6 +1,12 @@
+import { ReactNode } from "react";
+
 export interface IRouterType {
   title: string;
   path: string;
   element: JSX.Element;
   children?: IRouterType[];
+}
+
+export interface IBasicProps {
+  children?: ReactNode;
 }

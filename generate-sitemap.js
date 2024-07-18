9ed3e48953921415a567422dc2b9726548f87a63
secretariatv <oliver.b25.f@gmail.com>
2024-07-18 19:17:20 +0000

    update: sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,40 @@
+import { SitemapStream } from 'sitemap';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { resolve } from 'path';
+
+const pipelineAsync = promisify(pipeline);
+
+const baseUrl = 'https://portfolio-rho-gold-96.vercel.app';
+
+const paths = [
+  '/'
+];
+
+async function generateSitemap() {
+  try {
+    const sitemap = new SitemapStream({ hostname: baseUrl });
+
+    // Save the sitemap to a file
+    const sitemapPath = resolve('./public/sitemap.xml');
+    const writeStream = createWriteStream(sitemapPath);
+
+    // Write each path to the sitemap
+    paths.forEach((path) => {
+      sitemap.write({ url: path, changefreq: 'daily', priority: 0.7 });
+    });
+
+    sitemap.end();
+
+    // Use pipeline to handle the stream
+    await pipelineAsync(sitemap, writeStream);
+
+    console.log('Sitemap successfully created!');
+  } catch (err) {
+    console.error('Error generating sitemap:', err);
+  }
+}
+
+// Run the sitemap generation script
+generateSitemap();
